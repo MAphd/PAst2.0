b6c5ab589374d7001299b3cecb4197e793d809e3
MAphd <98038096+MAphd@users.noreply.github.com>
2022-08-11 13:32:21 +0200

    Path stuff
    Improved functionality when called from other directories than itself

--- PAST.R
+++ PAST.R
@@ -1,24 +1,36 @@
 
-#Load functions, must be in same dir as this script
-source("Functions/PAst2.0.R")
-source("Functions/WeightedAlignmentRouter.R")
-
-
-
-OsaDBdir <- "Functions/OSAdb.fasta"
 
 
 dargs <- list(i = "", o = "", b = FALSE)
-Param <- R.utils::commandArgs(defaults = dargs, trailingOnly = TRUE, asValues = TRUE)
+Param <- R.utils::commandArgs(defaults = dargs, trailingOnly = FALSE, always = list(file =""), asValues = TRUE)
 
+#Set correct path for some functions
+DIR2 <- dirname((Param$file))
+
+
+if( !dir.exists( (DIR2) ) ){
+  DIR3 <- ("./")
+} else {
+  DIR3 <- paste0( (DIR2),"/")
+}
+
+if( !dir.exists(DIR3) ){
+  stop(paste0("something went wrong with path ",DIR3))
+}
+
+
+
+OsaDBdir <- paste0(DIR3,"Functions/OSAdb.fasta")
+source(paste0(DIR3,"Functions/PAst2.0.R"))
+source(paste0(DIR3,"Functions/WeightedAlignmentRouter.R"))
 
 
 #Help message
 if( any(names(Param) %in% c("h","help") ) ){
   cat("PAst 2.0, a blast based tool for in silico serotyping of pseudomonas aeruginosa genomes\n
 Available commands:\n")
-  cat("-i Input directory path (Must be a file compatible with blastn with file extensions: .fasta, .fsa, .fas, .fna, .fa. or .seq). Defaults to input/ if not specified
--o Output directory path (defaults to output/ if not specified)
+  cat("-i Input genome(s) directory path Defaults to ./input/ in PAst2.0 folder if not specified
+-o Output directory path (defaults to . if not specified)
 -b (If you already previously performed the blast alignment for debugging purposes, defaults to FALSE)
 -p (To enable installation of missing packages required to run script)
 -h or -help Display this message\n")
@@ -35,9 +47,10 @@ for(i in 1:length(packages)){
   a <- suppressWarnings(suppressMessages( require( packages[i], character.only= TRUE) ))
   
   if( !a & (! any(names(Param) %in% "p") )){
-    stop(cat("Package", packages[i], "is missing, install package or run PAST2.0.R with flag -p to enable installation of packages\n"))
+    stop(cat("Package", packages[i], "is missing, install package or run PAST.R with flag -p to enable installation of packages\n"))
   } else if( !a & any(names(Param) %in% "p") ){
     install.packages(packages[i])
+    suppressWarnings(suppressMessages( require( packages[i]) ))
   }
   
 }
@@ -57,7 +70,7 @@ if( !identical( Param$i, "" ) ){
   }
   
 } else {
-  dInputquery <- "Input/"
+  dInputquery <- paste0(DIR3,"Input/")
   
   if( identical(list.files(dInputquery ,pattern = paste0(c("*fasta$","*fna$","*fsa$","*fas$","*fa$","*seq$"),collapse="|")), "as.character(0)") ){
     stop("Input folder has no valid files, input files must have file extension: .fasta, .fna, .fsa, .fas, .fa, or .seq")
@@ -82,7 +95,7 @@ if( !identical( Param$o, "" ) ){
   }
   
 } else {
-  dOutput <- "Output/"
+  dOutput <- paste0(dirname(dInputquery),"/PASToutput/" )
   
   if( !dir.exists(dOutput) ){
     dir.create(dOutput)
